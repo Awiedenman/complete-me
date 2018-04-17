61f8f68cf6448e15d2deeacad87deabcc7e22831
Theresa M Marquis <tmcjunkinmarquis@gmail.com>
2018-04-16 19:42:47 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-const Node = require('./scripts/Node');
-const Trie = require('./scripts/Trie');
+class Node {
+  constructor(letter = null) {
+   this.letter = letter;
+   this.isWord = false;
+   this.child = {};
+   this.weight = 0;
+  }
+}
 
 
+// const Node = require('./scripts/Node');
+// const Trie = require('./scripts/Trie');
 
-
-
-
-module.exports = { 
-    Node,
-    Trie 
-};
+// module.exports = { 
+//     Node,
+//     Trie 
+// };
 
